cf8f5e0c728f4126cb6cd8d224a0e22fdfd893a3
xschen <xs0040@gmail.com>
2017-12-08 11:19:47 +0800

    add csharp client

---
+++ csharp-client-proj/csharp-client-proj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebSocketSharp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_client_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://localhost:8080/my-ws"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Spring says: " + e.Data);
+
+                ws.Connect();
+                ws.Send("BALUS");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
